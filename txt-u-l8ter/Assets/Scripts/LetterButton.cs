b17ab79bb940e2f6eb4f81233d3a2a3728a60e9f
macarenzie <mhl8656@rit.edu>
2024-10-04 23:46:07 -0400

    got most of my code back in for the button

---
+++ txt-u-l8ter/Assets/Scripts/LetterButton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterButton : MonoBehaviour
+{
+    #region FIELDS
+
+    [SerializeField] private string[] letters;
+    private int letterIndex = 0;
+    private float pressBuffer = 0.5f;
+
+    // ui
+    [SerializeField] private TMP_Text displayText;
+    [SerializeField] private Button letterButton;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        letterButton.onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region METHODS
+
+    private void OnButtonClick()
+    {
+        letterIndex = (letterIndex + 1) % letters.Length;
+        OutputLetter(letters[letterIndex]);
+    }
+
+    private void OutputLetter(string letter)
+    {
+        displayText.text += letter;
+    }
+
+    #endregion
+}
